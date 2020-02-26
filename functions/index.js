a124b4ac00b128189bcc5df89003c97ad203f39b
Legapang <59070089@it.kmitl.ac.th>
2020-02-26 15:53:25 +0700

    Change url imagemap

--- functions/index.js
+++ functions/index.js
@@ -339,10 +339,10 @@ exports.webhook = functions
     // ส่วน function disease_imagemap ให้ผู้ใช้กดเลือกอาการจากแผนภาพเพื่อหาโรคที่เข้าข่าย ------ [2.2]
     const disease_imagemap = async => {
       // แสดงส่วนที่เกิดโรค
-      const carouselMsg = {
+      const imagemapMsg = {
         "line": {
         "type": "imagemap",
-        "baseUrl": "https://firebasestorage.googleapis.com/v0/b/chatbot-baac-cdplft.appspot.com/o/104.png?alt=media&token=332ec3fb-c09b-47e5-bf58-bf5c34e9e562#?width=auto",
+        "baseUrl": "https://c1.staticflickr.com/5/4682/38602688145_eb29f0f180_o.png",
         "altText": "This is an imagemap",
         "baseSize": {
           "width": 1040,
@@ -412,7 +412,7 @@ exports.webhook = functions
         ]}
       };
 
-      const payloadMsg = new Payload("LINE", carouselMsg, {
+      const payloadMsg = new Payload("LINE", imagemapMsg, {
         sendAsMessage: true
     });
     return agent.add(payloadMsg);

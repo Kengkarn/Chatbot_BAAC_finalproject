7119d25d03784540d8dc5e12f615c384a167855a
Legapang <59070089@it.kmitl.ac.th>
2020-03-10 07:14:20 +0700

    const -> let in leaf_select

--- functions/index.js
+++ functions/index.js
@@ -1014,7 +1014,7 @@ exports.webhook = functions
         return agent.add(leaf_symptom);
       }
       else if (leaf_symptom == "ใบซีดเหลือง") {
-        const disease_name = db.collection('Disease_new').doc('Leaf').collection('symptom').doc('yellow').get().then(doc => {
+        let disease_name = db.collection('Disease_new').doc('Leaf').collection('symptom').doc('yellow').get().then(doc => {
           agent.add(doc.data().diseaseName);
         });
         /*return db.collection('Disease').doc(disease_name).get().then(doc => {
@@ -1030,7 +1030,7 @@ exports.webhook = functions
                     "imageUrl": "https://example.com/bot/images/item1.jpg",
                     "action": {
                       "type": "postback",
-                      "label": "disease_name",
+                      "label": disease_name,
                       "data": "action=buy&itemid=111"
                     }
                   },
@@ -1038,8 +1038,8 @@ exports.webhook = functions
                     "imageUrl": "https://example.com/bot/images/item2.jpg",
                     "action": {
                       "type": "message",
-                      "label": "disease_name",
-                      "text": "disease_name"
+                      "label": disease_name,
+                      "text": disease_name
                     }
                   }
               ]
